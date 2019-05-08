efe9c74a46b23a25273e2e36e2926f289b5666d2
Swapnil Joshi (MAS) <SW20064149@wipro.com>
2019-05-08 15:51:54 +0530

    Added new field to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,10 @@ request({ url, json: true }, (error, {body}) => {
          callback('Unable to find the location',undefined)
      }
      else {
-        callback(undefined,body.daily.data[0].summary+ ' It is curently '+body.currently.temperature+' degress out. There is a '+body.currently.precipProbability+'% chance of rain.')
+        callback(undefined,body.daily.data[0].summary+ ' It is curently '+body.currently.temperature+
+                           ' degress out. There is a '+body.currently.precipProbability+'% chance of rain.'
+                           +' \n The High temperature of the day is '+ body.daily.data[0].temperatureHigh +
+                           ' degrees. The Low temperature of the day is '+ body.daily.data[0].temperatureLow + ' degrees.')
      }
    
 })
